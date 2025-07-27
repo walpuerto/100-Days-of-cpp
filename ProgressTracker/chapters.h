6d32e19341dfadaf6b97a51877173eee58981dce
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2025-07-27 13:41:04 +0800

    Added "quiz" into the funciton comments.

--- ProgressTracker/chapters.h
+++ ProgressTracker/chapters.h
@@ -4,10 +4,10 @@
 // Call to display the first 13 subchapters of the program.
 void chapter0();
 
-// Call to display the next 10 subchapters of the program.
+// Call to display the next 10 subchapters and one quiz of the program.
 void chapter1();
 
-// Call to display the next 13 subchapters of the program.
+// Call to display the next 13 subchapters and one quiz of the program.
 void chapter2();
 
 // Call to display the latest subchapters studied by the author.
