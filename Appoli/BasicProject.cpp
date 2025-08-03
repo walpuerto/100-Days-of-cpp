a991a1c62a8b8e9c63a3c770a6d5d027348f81f5
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2025-08-03 16:34:17 +0800

    Day 18 (Part 1)

---
+++ Appoli/BasicProject.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string_view>
+
+// Write the function getQuantityPhrase() here
+std::string_view getQuantityPhrase(int quantity)
+{
+    if (quantity < 0)
+        return "negative";
+    if (quantity == 0)
+        return "no";
+    if (quantity == 1)
+        return "a single";
+    if (quantity == 2)
+        return "a couple of";
+    if (quantity == 3)
+        return "a few";
+    if (quantity > 3)
+        return "many";
+    return "";
+}
+
+// Write the function getApplesPluralized() here
+std::string_view getApplesPluralized(int quantity)
+{
+    return ((quantity == 1) ? "apple" : "apples");
+}
+
+
+int main()
+{
+    constexpr int maryApples{ 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
+}
